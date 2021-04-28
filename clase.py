d3e37987bdae3159fd99a4b9f2bb6c3cba5fa4c7
victorteo0497 <82296527+victorteo0497@users.noreply.github.com>
2021-04-28 15:14:09 +0300

    Add files via upload

---
+++ clase.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+print("Aici incepe")
+cilass Persoana():
+
+	nr_persoane = 0
+
+	def __init__(self , nume, varsta):
+		self.nume = nume
+		self.varsta = varsta
+		self.nr_persoane = self.nr_persoane + 1
+
+	def setVarsta(self, varsta):
+		self.varsta = varsta
+
+	def setNume(self, nume):
+		self.nume = nume	
+
+	def getNume(self):
+		return self.nume
+
+	def getVarsta(self):
+		return self.varsta	
+
+p1 = Persoana("Adi", 23)
+p2 = Persoana("Ramon", 24)
+
+print(p1.getNume())
+print(p1.getVarsta())
+
+p1.setVarsta(50)
+p1.setNume("Adrian")
+
+print(p1.getNume())
+print(p1.getVarsta())
+
+print(p2.getNume())
+print(p2.getVarsta())
+
+
+
